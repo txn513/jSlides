7f7c70e9994a78775da97f3d984b48c40e95ab06
txn513 <xtian513@gmail.com>
2015-07-23 02:46:14 +0800

    add options

--- demo/jSlides.js
+++ demo/jSlides.js
@@ -10,28 +10,24 @@
 				img2: {h2:'Friendly', h3:'DEVIL'},
 				img3: {h2:'Tranquilent', h3:'COMPATRIOT'},
 				img4: {h2:'Insecure', h3:'HUSSLER'},
-				img5: {h2:'Tranquilent', h3:'COMPATRIOT'}
+				autoplay: false,
+				time: 3000
 			}
+			options = $.extend(settings, options);
 			var objNum = Object.getOwnPropertyNames(settings).length;
-			// $.extend(settings, options);
 			
 			$(this).append(mainDiv).append(ctrlDiv);
 
-			function g(id){
+			
+
+			var _g = function(id){
 			if ( id.substr(0,1) == '.'){
 						return document.getElementsByClassName(id.substr(1));
 					}
 				return document.getElementById(id);
 			}
 
-			function addSlides (){
-				var data = [
-						{img:1, h2:'Creative', h3:'DUET'},
-						{img:2, h2:'Friendly', h3:'DEVIL'},
-						{img:3, h2:'Tranquilent', h3:'COMPATRIOT'},
-						{img:4, h2:'Insecure', h3:'HUSSLER'}
-					];
-
+			var _addSlides = function(){
 				var main_arr = [];
 				var ctrl_arr = [];
 								
@@ -41,8 +37,8 @@
 					_html_main.append('<div class="main-i" id="main-'+ i +'"></div>');
 					_html_main.children('.main-i').append($(img[i]).clone(true))
 					.append('<div class="caption">'
-					+ '<h2>'+ data[i].h2 +'</h2>'
-					+ '<h3>'+ data[i].h3 +'</h3></div>');
+					+ '<h2>'+ options['img'+(i+1)].h2 +'</h2>'
+					+ '<h3>'+ options['img'+(i+1)].h3 +'</h3></div>');
 
 					var _html_ctrl = $('<div class="ctrl" id="ctrl-temp"><a href="javascript:;" class="ctrl-i" id="ctrl-'
 					+ i +'" index="'+ i +'"></a></div>');
@@ -56,39 +52,49 @@
 			
 			}
 
-			function fnClick(){
-			var j = 0;
-			for(var i=0; i<$('.ctrl-i').length; i++){
-				
-				$('.ctrl-i')[i].onclick = function(){
-					j = this.getAttribute('index');
-					switchSlides(j)
+			var _fnClick = function(){
+				var j = 0;
+				for(var i=0; i<$('.ctrl-i').length; i++){
+					
+					$('.ctrl-i')[i].onclick = function(){
+						j = this.getAttribute('index');
+						_switchSlides(j)
+					}
 				}
 			}
-		}
-		function switchSlides (n){
-			var main= g('main-'+n);
-			var ctrl= g('ctrl-'+n);
-			
-			var clear_main = g('.main-i');
-			var clear_ctrl = g('.ctrl-i');
-
-			for(var i = 0; i < clear_main.length; i++){
+			var _switchSlides = function(n){
+				var main= _g('main-'+n);
+				var ctrl= _g('ctrl-'+n);
 				
-				clear_main[i].className = "main-i";
-				clear_ctrl[i].className = "ctrl-i";
+				var clear_main = _g('.main-i');
+				var clear_ctrl = _g('.ctrl-i');
+
+				for(var i = 0; i < clear_main.length; i++){
+					
+					clear_main[i].className = "main-i";
+					clear_ctrl[i].className = "ctrl-i";
+				}
+				main.className += " active";
+				ctrl.className += " active";
 			}
-			main.className += " active";
-			ctrl.className += " active";
-		}
-			
-			addSlides();
-			switchSlides(1);
-			fnClick();
+			var _autoPlay = function(){
+				var i = 0;
+				setInterval(function(){
+					_switchSlides(i);
+					if(i == img.length-1){
+						i = 0;
+					}else{
+						i++;
+					}
+				}, settings.time);
+			}
+			_addSlides();
+			_switchSlides(0);
+			_fnClick();
+			if(options.autoplay){
+				_autoPlay();
+			};
 		});
-   		
-		
-		
     };
  
 }( jQuery ));
